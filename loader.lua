f5fd2e96ac672d24746b88b7ea966125a31dad30
DookDekDEE <75850456+DookDekDEE@users.noreply.github.com>
2024-12-07 03:56:25 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -2,5 +2,5 @@ local idgame = game.PlaceId
 if idgame == 4520749081 or idgame == 6381829480 or idgame == 15759515082 or idgame == 5931540094 then
   loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/b4edb3d861d4121e6aa6aaab93cb3390.lua"))()
 elseif idgame == 9921477928 then
-
+  loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/8ffc5fcf6bf436062657116873238dc4.lua"))()
 end
