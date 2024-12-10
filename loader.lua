3638f4257dbb87b316b5e937581a25eb87fab6cd
DookDekDEE <75850456+DookDekDEE@users.noreply.github.com>
2024-12-10 08:10:19 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -3,4 +3,6 @@ if idgame == 4520749081 or idgame == 6381829480 or idgame == 15759515082 or idga
   loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/b4edb3d861d4121e6aa6aaab93cb3390.lua"))()
 elseif idgame == 9921477928 then
   loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/8ffc5fcf6bf436062657116873238dc4.lua"))()
+elseif idgame == 89438510123061 then
+  loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/8ca4ec399bb51f74879cbdef584641ce.lua"))()
 end
