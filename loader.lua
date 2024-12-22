a7e0a1b40b2b3ee690f16ae1c9de19e1394d43a4
DookDekDEE <75850456+DookDekDEE@users.noreply.github.com>
2024-12-22 09:17:15 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,8 +1,8 @@
 local idgame = game.PlaceId
 if idgame == 4520749081 or idgame == 6381829480 or idgame == 15759515082 or idgame == 5931540094 then
-  loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/b4edb3d861d4121e6aa6aaab93cb3390.lua"))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/DookDekDEE/All-Script/refs/heads/main/kingl.lua"))()
 elseif idgame == 9921477928 then
-  loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/8ffc5fcf6bf436062657116873238dc4.lua"))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/DookDekDEE/All-Script/refs/heads/main/jj.lua"))()
 elseif idgame == 89438510123061 then
-  loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/8ca4ec399bb51f74879cbdef584641ce.lua"))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/DookDekDEE/All-Script/refs/heads/main/shadow.lua"))()
 end
