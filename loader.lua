7fea4eab6b447a7b33e2323db0caadcdd8b4e59a
DookDekDEE <75850456+DookDekDEE@users.noreply.github.com>
2025-06-20 07:08:48 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -20,6 +20,6 @@ elseif idgame == 111989938562194 then
 elseif idgame == 17850641257 or idgame == 17850769550 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/DookDekDEE/All-Script/main/saga.lua"))()
 elseif idgame == 136755111277466 then
-  loadstring(game:HttpGet("https://pandadevelopment.net/virtual/file/37efb17d1f46d911"))()
+  loadstring(game:HttpGet("https://pandadevelopment.net/virtual/file/d7b470a4b9090456"))()
   --loadstring(game:HttpGet("https://raw.githubusercontent.com/DookDekDEE/All-Script/main/shadow2.lua"))()
 end
